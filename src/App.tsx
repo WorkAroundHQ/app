9961839e95bc387ad78b248383b98a6830e9f403
Maurice Gerhardt <maurice.gerhardt@code.berlin>
2021-09-09 16:58:11 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './assets/logo.webp'
+import './scss/app.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
+        <h2>WorkAround App</h2>
+        <p>We're currently developing an application for digital nomads here.</p>
+        <p>Come back soon...</p>
+        <div className="social-media">
+          <a href="https://github.com/WorkAroundHQ">GitHub</a>
+          <a href="https://twitter.com/WorkAroundHQ">Twitter</a>
+        </div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

9da69a67807416cbab344dc4cfd42ccea95c4439
Maurice Gerhardt <maurice.gerhardt@code.berlin>
2021-09-10 10:16:35 +0200

    link landing page

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ function App() {
     <div className="app">
       <header className="app-header">
         <img src={logo} className="app-logo" alt="logo" />
-        <h2>WorkAround App</h2>
-        <p>We're currently developing an application for digital nomads here.</p>
-        <p>Come back soon...</p>
+        <h2>WorkAround</h2>
+        <p>Hii, we're currently developing an application for digital nomads.</p>
+        <p>While we're busy with coding, please check out our <a href="https://www.workaround.world">website</a></p>
         <div className="social-media">
           <a href="https://github.com/WorkAroundHQ">GitHub</a>
           <a href="https://twitter.com/WorkAroundHQ">Twitter</a>

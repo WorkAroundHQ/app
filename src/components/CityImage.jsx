ac25080ce8a16c02474ab08a493d2a53fb49d6ba
Maurice <maurice.gerhardt@code.berlin>
2021-11-04 12:00:24 +0100

    feat: retrieve signed sources
    - fix srcSet tag

--- src/components/CityImage.jsx
+++ src/components/CityImage.jsx
@@ -1,16 +1,16 @@
 const CityImage = (city, imageSources) => {
 	return (
 	<picture className='city-image'>
-			<source type="image/webp" media="(min-width: 56.25rem)" srcset={imageSources.lWebp} />
-			<source type="image/jpeg" media="(min-width: 56.25rem)" srcset={imageSources.lJpg} />
+			<source type="image/webp" media="(min-width: 56.25rem)" srcSet={imageSources.lWebp} />
+			<source type="image/jpeg" media="(min-width: 56.25rem)" srcSet={imageSources.lJpg} />
 
-			<source type="image/webp" media="(min-width: 37.5rem)" srcset={imageSources.mWebp} />
-			<source type="image/jpeg" media="(min-width: 37.5rem)" srcset={imageSources.mJpg} />
+			<source type="image/webp" media="(min-width: 37.5rem)" srcSet={imageSources.mWebp} />
+			<source type="image/jpeg" media="(min-width: 37.5rem)" srcSet={imageSources.mJpg} />
 
-			<source type="image/webp" srcset={imageSources.sWebp} />
-			<source type="image/jpeg" srcset={imageSources.sJpg} />
+			<source type="image/webp" srcSet={imageSources.sWebp} />
+			<source type="image/jpeg" srcSet={imageSources.sJpg} />
 
-			<img src={imageSources.mJpg} style="width: 100%;" alt={city} />
+			<img src={imageSources.mJpg} alt={city} />
 	</picture>
 	)
 }
--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -3,60 +3,86 @@ import { supabase } from '../supabaseClient'
 import CityImage from '../components/CityImage'
 
 const Cities = () => {
-	const [imagePaths, setImagePaths] = useState(null)
-	const [imageSources, setImageSources] = useState(null)
-  const [uploading, setUploading] = useState(false)
 	const cityFolder = 'europe/germany/berlin/'
+	const [imagePaths, setImagePaths] = useState([])
+	const [imageSources, setImageSources] = useState([])
+	const [imageUrl, setImageUrl] = useState(null)
 
 	useEffect(() => {
+		console.log('city folder hook')
     if (cityFolder) getImagePaths(cityFolder)
   }, [cityFolder])
 
 	useEffect(() => {
-    if (imagePaths) getSignedImageSources(imagePaths)
+		console.log('image path hook')
+    if (imagePaths) getSignedUrl(imagePaths)
   }, [imagePaths])
+
+	useEffect(() => {
+		console.log('image source hook')
+    if (imageSources) console.log(imageSources)
+  }, [imageSources])
+
+	const getImgKey = (imgName) => {
+		const imgSize = imgName.split('-')[0]
+		let imgType = imgName.split('.')[1]
+		imgType = imgType[0].toUpperCase() + imgType.substring(1)
+		return `${imgSize}${imgType}`
+	}
+
+	const getSignedUrl = async (paths) => {
+		let neww = []
+		for (const path of paths) {
+			try {
+				const { data, error } = await supabase.storage.from('city-images').createSignedUrl(path, 3600)
+				if (error) {
+					throw error
+				}
+				neww = [...neww, data.signedURL]
+				
+			} catch (error) {
+				console.log('Error getting signed url: ', error.message)
+			}
+		}
+		setImageSources(neww)
+  }
 
 	const getImagePaths = async (cityFolder) => {
 		try {
-			const { data, error } = await supabase.storage.from('city-images').list(cityFolder)
-			if (error) {
-				throw error
+      const { data, error } = await supabase.storage.from('city-images').list(cityFolder)
+      if (error) {
+        throw error
+      }
+
+			const imagePaths = data.map(img => {
+				return `${cityFolder}${img.name}`
+			})
+
+			// console.log(imagePaths)
+      setImagePaths(imagePaths)
+    } catch (error) {
+      console.log('Error getting signed url: ', error.message)
+    }
+	}
+
+	const getSignedImageSources = (cityFolder, image) => {
+		supabase.storage.from('city-images').createSignedUrl(`${cityFolder}${image.name}`, 3600)
+		.then((res) => {
+			try {
+				if (res.error) throw res.error
+				const imgKey = getImgKey(image.name)
+				return {[imgKey]: res.signedURL}
+			} catch (error) {
+				console.log('Error getting image paths: ', error.message)
 			}
-
-			const imagePaths = data.map(image => {
-				return `${cityFolder}${image.name}`
-			})
-			console.log(imagePaths)
-			setImagePaths(imagePaths)
-		} catch (error) {
-			console.log('Error getting image paths: ', error.message)
-		}
+		})
 	}
 
-	const getSignedImageSources = async (imagePaths) => {
-		let signedImageSources = {}
-
-		imagePaths.forEach(async path => {
-				console.log(`Path: ${path}`)
-				try {
-					const { signedURL, error } = await supabase.storage.from('city-images').createSignedUrl(path, 3600)
-					if (error) {
-						throw error
-					}
-					signedImageSources[path] = signedURL
-				} catch (error) {
-					console.log('Error getting image paths: ', error.message)
-				}
-			})
-			
-		console.log(signedImageSources)
-		setImageSources(signedImageSources)
-	}
-	
 	return (
 		<section id='cities'>
 			<h1>Cities</h1>
-
+			<CityImage city='Berlin' imageSources={imageSources} />
+			<img src={imageUrl} alt='Berlin' />
 		</section>
 	)
 }

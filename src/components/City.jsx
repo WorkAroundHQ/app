942def243c12209ad2ee288e048e6c7ed0878ff9
Maurice <maurice.gerhardt@code.berlin>
2021-11-05 14:03:36 +0100

    feat: Host images directly for better performance
    Instead of retrieving images from supabase I host the now directly which is way faster

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -10,50 +10,25 @@ const City = ({ city }) => {
 	useEffect(() => {
 		if (!loaded) getImagePaths(city.bucket_folder)
   })
+	
+	const getImgKey = (size, type) => {
+		type = type[0].toUpperCase() + type.substring(1)
+		return `${size}${type}`
+	}
 
 	const getImagePaths = async (cityFolder) => {
-		const imagePaths = []
-		try {
-      const { data, error } = await supabase.storage.from('city-images').list(cityFolder)
-      if (error) {
-        throw error
-      }
-			for (const img of data) {
-				imagePaths.push(`${cityFolder}${img.name}`)
-			}
-			setLoaded(true)
-      getSignedUrl(imagePaths)
-    } catch (error) {
-      console.log('Error getting signed url: ', error.message)
-    }
-	}
-
-	const getImgKey = (path) => {
-		const splittedPath = path.split('/')
-		const fileName = splittedPath[splittedPath.length - 1]
-		const imgSize = fileName.split('-')[0]
-		let imgType = fileName.split('.')[1]
-		imgType = imgType[0].toUpperCase() + imgType.substring(1)
-		return `${imgSize}${imgType}`
-	}
-
-	const getSignedUrl = async (paths) => {
-		let imgSources = {}
-		for (const path of paths) {
-			const fileName = getImgKey(path)
-			try {
-				console.info(`GET SIGNED URL: ${path}`)
-				const { data, error } = await supabase.storage.from('city-images').createSignedUrl(path, 3600)
-				if (error) {
-					throw error
-				}
-				imgSources = {...imgSources, [fileName]: data.signedURL}
-			} catch (error) {
-				console.log('Error getting signed url: ', error.message)
+		const imagePaths = {}
+		const sizes = ['s', 'm', 'l']
+		const types = ['webp', 'jpg']
+		for (const type of types) {
+			for (const size of sizes) {
+				let image = await import(`../assets/city-images/${cityFolder}${size}.${type}`)
+				imagePaths[getImgKey(size, type)] = image.default
 			}
 		}
-		setImageSources(imgSources)
-  }
+		setLoaded(true)
+		setImageSources(imagePaths)
+	}
 
 	return (
 		<div className='city'>

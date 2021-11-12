fb735a85a9c90b4a1b5346018239c0b7bbfb5bc6
Maurice <maurice.gerhardt@code.berlin>
2021-11-12 16:04:04 +0100

    feat: Add test that checks data from mock sever

--- src/tests/Profile.test.js
+++ src/tests/Profile.test.js
@@ -1,12 +1,12 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen, waitFor } from "@testing-library/react"
 import Profile from "../pages/Profile"
 
 import getSession from './helper/get-session'
 
-test('that the edit button is active by default', () => {
+test('that the edit button is active by default', async () => {
 	render(<Profile session={getSession()} />)
 
-	const editBtn = screen.getByRole('button', { name: /^Edit$/ })
+	const editBtn = await waitFor(() => screen.getByRole('button', { name: /^Edit$/ }))
 
 	expect(editBtn).toBeEnabled()
 })
@@ -35,19 +35,43 @@ test('that all form inputs are disabled by default', () => {
 	expect(aboutInput).toBeDisabled()
 })
 
-test('that on edit button click it changes to save button', () => {
+test('that all form inputs have the correct value', async () => {
 	render(<Profile session={getSession()} />)
 
-	const editBtn = screen.getByRole('button', { name: /^Edit$/ })
+	const emailInput = await waitFor(() => screen.getByDisplayValue('tim.apple@pear.com'))
+	const nameInput = await waitFor(() => screen.getByDisplayValue('Tim Apple'))
+	const jobInput = await waitFor(() => screen.getByDisplayValue('Pear CEO'))
+	const twitterInput = await waitFor(() => screen.getByDisplayValue('applePearTwit'))
+	const instagramInput = await waitFor(() => screen.getByDisplayValue('applePearInsta'))
+	const linkedinInput = await waitFor(() => screen.getByDisplayValue('tim-apple'))
+	const githubInput = await waitFor(() => screen.getByDisplayValue('applePearHubber'))
+	const openInput = screen.getByLabelText('Open for work?')
+	const aboutInput = await waitFor(() => screen.getByDisplayValue('I\'m Tim Apple, Pear CEO'))
+
+	expect(emailInput).toHaveAttribute('id', 'email')
+	expect(nameInput).toHaveAttribute('id', 'name')
+	expect(jobInput).toHaveAttribute('id', 'job')
+	expect(twitterInput).toHaveAttribute('id', 'twitter')
+	expect(instagramInput).toHaveAttribute('id', 'instagram')
+	expect(linkedinInput).toHaveAttribute('id', 'linkedin')
+	expect(githubInput).toHaveAttribute('id', 'github')
+	expect(openInput).not.toBeChecked()
+	expect(aboutInput).toHaveAttribute('id', 'bio')
+})
+
+test('that on edit button click it changes to save button', async () => {
+	render(<Profile session={getSession()} />)
+
+	const editBtn = await waitFor(() => screen.getByRole('button', { name: /^Edit$/ }))
 
 	editBtn.click()
 	expect(editBtn).toHaveTextContent(/^Save$/)
 })
 
-test('that on edit button click the inputs get enabled (except email)', () => {
+test('that on edit button click the inputs get enabled (except email)', async () => {
 	render(<Profile session={getSession()} />)
 
-	const editBtn = screen.getByRole('button', { name: /^Edit$/ })
+	const editBtn = await waitFor(() => screen.getByRole('button', { name: /^Edit$/ }))
 	const emailInput = screen.getByLabelText('Email')
 	const nameInput = screen.getByLabelText('Name')
 	const jobInput = screen.getByLabelText('Job Description')
@@ -70,10 +94,10 @@ test('that on edit button click the inputs get enabled (except email)', () => {
 	expect(aboutInput).toBeEnabled()
 })
 
-test('that the upload button is only visible while editing', () => {
+test('that the upload button is only visible while editing', async () => {
 	render(<Profile session={getSession()} />)
 
-	const editBtn = screen.getByRole('button', { name: /^Edit$/ })
+	const editBtn = await waitFor(() => screen.getByRole('button', { name: /^Edit$/ }))
 	const uploadBtn = screen.getByTestId('upload-button')
 	
 	expect(uploadBtn).not.toBeVisible()

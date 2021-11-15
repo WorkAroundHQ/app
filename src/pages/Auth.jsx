56011d4d2f00f71a10c82bf3b2244c91c2bf8895
Maurice <maurice.gerhardt@code.berlin>
2021-11-15 11:02:35 +0100

    feat: add error messages on login / sign up page

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -11,19 +11,28 @@ const Auth = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [repeatedPassword, setRepeatedPassword] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
 
 	const handleLogin = async (email, password) => {
-    try {
-      setLoading(true)
-      const { error } = await supabase.auth.signIn({
-        email: email,
-        password: password
-      })
-      if (error) throw error
-    } catch (error) {
-      alert(error.error_description || error.message)
-    } finally {
-      setLoading(false)
+    if (email.length !== 0) {
+      if (password.length !== 0) {
+        try {
+          setLoading(true)
+          const { error } = await supabase.auth.signIn({
+            email: email,
+            password: password
+          })
+          if (error) throw error
+        } catch (error) {
+          setErrorMessage(error.error_description || error.message)
+        } finally {
+          setLoading(false)
+        }
+      } else {
+        setErrorMessage('Please enter your password')
+      }
+    } else {
+      setErrorMessage('Please enter your email')
     }
   }
 
@@ -39,25 +48,25 @@ const Auth = () => {
           if (error) throw error
           alert('Check your email for the confirmation link!')
         } catch (error) {
-          alert(error.error_description || error.message)
+          setErrorMessage(error.error_description || error.message)
         } finally {
           setLoading(false)
         }
       } else {
-        alert('The passwords are not equal. Please try again')
+        setErrorMessage('The passwords are not equal. Please try again')
       }
     } else {
-      alert('The password is not long enough. At least 8 characters are required')
+      setErrorMessage('The password is not long enough. At least 8 characters are required')
     }
   }
 
   const handlePrimaryButtonClick = async (e) => {
-      e.preventDefault()
-      if (signUpMode) {
-        handleSignup(email, password, repeatedPassword)
-      } else {
-        handleLogin(email, password)
-      }
+    e.preventDefault()
+    if (signUpMode) {
+      handleSignup(email, password, repeatedPassword)
+    } else {
+      handleLogin(email, password)
+    }
   }
 
   const handleEnterPress = (e) => {
@@ -74,13 +83,17 @@ const Auth = () => {
       </div>
       <div className="auth-form">
         <div className='auth-inputs'>
-            <Input type='email' placeholder='Your email' onChange={(e) => setEmail(e.target.value)} onEnter={(e) => handleEnterPress(e)} />
-            <Input type='password' placeholder='Password' onChange={(e) => setPassword(e.target.value)} onEnter={(e) => handleEnterPress(e)} />
-            {signUpMode ? 
-              <Input type='password' placeholder='Repeat Password' onChange={(e) => setRepeatedPassword(e.target.value)} onEnter={(e) => handleEnterPress(e)} /> : ''
-            }
-          </div>
-          <Button text={loading ? 'Loading' : signUpMode ? 'Sign Up' : 'Login'} mode='primary' disabled={loading} onClick={e => handlePrimaryButtonClick(e)} />
+          <Input type='email' placeholder='Your email' onChange={(e) => setEmail(e.target.value)} onEnter={(e) => handleEnterPress(e)} />
+          <Input type='password' placeholder='Password' onChange={(e) => setPassword(e.target.value)} onEnter={(e) => handleEnterPress(e)} />
+          {signUpMode ? 
+            <Input type='password' placeholder='Repeat Password' onChange={(e) => setRepeatedPassword(e.target.value)} onEnter={(e) => handleEnterPress(e)} /> : ''
+          }
+        </div>
+        <Button text={loading ? 'Loading' : signUpMode ? 'Sign Up' : 'Login'} mode='primary' disabled={loading} onClick={e => handlePrimaryButtonClick(e)} />
+        <div className={`auth-error${errorMessage === '' ? ' hidden' : ' visible'}`}>
+          <ion-icon name="alert-circle"></ion-icon>
+          <p className='auth-error-text'>{errorMessage}</p>
+        </div>
       </div>
       </div>
         <div className="auth-footer">
@@ -88,6 +101,7 @@ const Auth = () => {
             <button onClick={(e) => {
               e.preventDefault()
               setAuthMode(!signUpMode)
+              setErrorMessage('')
             }}>here</button>
           </p>
         </div>

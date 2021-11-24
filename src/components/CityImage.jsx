9cf4be9dd00f67d04a6ae9b78feb13dc07a7f9a7
Maurice <maurice.gerhardt@code.berlin>
2021-11-24 17:57:37 +0100

    feat: use correct breakpoints

--- src/components/CityImage.jsx
+++ src/components/CityImage.jsx
@@ -1,17 +1,17 @@
 const CityImage = ({ city, imageSources }) => {
 	return (
-	<picture>
-			<source type="image/webp" media="(min-width: 56.25rem)" srcSet={imageSources.lWebp} />
-			<source type="image/jpeg" media="(min-width: 56.25rem)" srcSet={imageSources.lJpg} />
-
-			<source type="image/webp" media="(min-width: 37.5rem)" srcSet={imageSources.mWebp} />
-			<source type="image/jpeg" media="(min-width: 37.5rem)" srcSet={imageSources.mJpg} />
-
-			<source type="image/webp" srcSet={imageSources.sWebp} />
-			<source type="image/jpeg" srcSet={imageSources.sJpg} />
-
-			<img className='city-image' src={imageSources.mJpg} alt={city} />
-	</picture>
+		<picture>
+				<source type="image/webp" media="(min-width: 63.25rem)" srcSet={imageSources.lWebp} />
+				<source type="image/jpeg" media="(min-width: 63.25rem)" srcSet={imageSources.lJpg} />
+	
+				<source type="image/webp" media="(min-width: 34rem)" srcSet={imageSources.mWebp} />
+				<source type="image/jpeg" media="(min-width: 34rem)" srcSet={imageSources.mJpg} />
+	
+				<source type="image/webp" srcSet={imageSources.sWebp} />
+				<source type="image/jpeg" srcSet={imageSources.sJpg} />
+	
+				<img className='city-image' src={imageSources.mJpg} alt={city} />
+		</picture>
 	)
 }
 

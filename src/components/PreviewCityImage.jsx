f2a4c82761f02ca2848328db05a6e0d135ab243e
Maurice <maurice.gerhardt@code.berlin>
2021-11-24 17:56:51 +0100

    ref: fix indentation

--- src/components/PreviewCityImage.jsx
+++ src/components/PreviewCityImage.jsx
@@ -1,11 +1,11 @@
 const PreviewCityImage = ({ city, imageSources }) => {
 	return (
-	<picture>
-			<source type="image/webp" srcSet={imageSources.sWebp} />
-			<source type="image/jpeg" srcSet={imageSources.sJpg} />
-
-			<img className='preview-city-image' src={imageSources.sJpg} alt={city} />
-	</picture>
+		<picture>
+				<source type="image/webp" srcSet={imageSources.sWebp} />
+				<source type="image/jpeg" srcSet={imageSources.sJpg} />
+	
+				<img className='preview-city-image' src={imageSources.sJpg} alt={city} />
+		</picture>
 	)
 }
 
